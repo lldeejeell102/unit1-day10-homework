9880e11a2c2740edbef416312b2f82ec83353441
Daniel Ji <daniel.ji0923@gmail.com>
2023-11-30 19:44:14 -0800

    I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -10,5 +10,20 @@ $(() => {
     // console.log($h1)
     $h1.text("Hogwarts")
     $container.append($h1)
-    // $h1.append("Hogwarts")
+
+    const $h2 = $("<h2>")
+    $h2.text("Daniel")
+    $h1.append($h2)
+    
+    const $h3 = $("<h3>")
+    $h3.text("Hufflepuff")
+    $h2.append($h3)
+    
+    const $h4 = $("<h4>")
+    $h4.text("Gigi").addClass("owl")
+    $h3.append($h4)
+
+    const $h4Wand = $("<h4>")
+    $h4Wand.text("Holly Wand with Unicorn Hair Core")
+    $h4.append($h4Wand)
 });
