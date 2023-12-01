94ec51fbe20b1954129b77b008550fea0bfed711
Daniel Ji <daniel.ji0923@gmail.com>
2023-11-30 19:32:21 -0800

    I added my first element and it was fun

--- app.js
+++ app.js
@@ -2,3 +2,13 @@
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
 
+$(() => {
+	// put jQuery in here
+    const $container = $("#container")
+    // console.log($container)
+    const $h1 = $("<h1>")
+    // console.log($h1)
+    $h1.text("Hogwarts")
+    $container.append($h1)
+    // $h1.append("Hogwarts")
+});
